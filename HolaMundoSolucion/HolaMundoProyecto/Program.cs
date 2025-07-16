f47ebe82c4a10cd3b8c4df5d5e31ea956c9a6be8
Jorge-Martinez82 <jmartines@gmail.com>
2025-07-16 11:36:13 +0200

    Primer Hola Mundo

---
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("============ ");
+
+        Console.WriteLine(@"  ___  ___  ___  ___  ___.---------------.");
+        Console.WriteLine(@".'\__\'\__\'\__\'\__\'\__,`   .  ____ ___ \");
+        Console.WriteLine(@"|\/ __\/ __\/ __\/ __\/ _:\   |`.  \  \___ \");
+        Console.WriteLine(@" \\'\__\'\__\'\__\'\__\'\_`.__|""`. \  \___ \");
+        Console.WriteLine(@"  \\/ __\/ __\/ __\/ __\/ __:                \");
+        Console.WriteLine(@"   \\'\__\'\__\'\__\ \__\'\_;-----------------`");
+        Console.WriteLine(@"    \\/   \/   \/   \/   \/ :               hh|");
+        Console.WriteLine(@"     \|______________________;________________|");
+         
+         
+    }
+}
